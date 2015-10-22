8031a0aade5283350c03f8bb1f0ec0a283bab4a0
Dan Nixon <dan@dan-nixon.com>
2015-10-22 11:26:41 +0100

    Add Maven project and CI

---
+++ coursework1/csc3423-cw1/src/main/java/com/dan_nixon/csc3423/cw1/App.java
@@ -0,0 +1,13 @@
+package com.dan_nixon.csc3423.cw1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
